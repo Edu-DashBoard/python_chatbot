c18e7c3e34c1d32748a17f01c91cd3af059e1d37
namnamu <skadlsrud8@naver.com>
2023-11-09 20:02:26 +0900

    Post left or right

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,6 +7,8 @@ from django.template import loader
 from main import API
 
 user_input= ""
+user_output=""
+user_output2=""
 def index(request):
     #edit
     company_list = Company.objects.all()		# Company 모델에 있는 정보를 전부 가져온다.
@@ -17,7 +19,11 @@ def index(request):
 def save_user_input(request):
     company_list = Company.objects.all()  # Company 모델에 있는 정보를 전부 가져옵니다.
     context = {'company_list': company_list}
+    
     global user_input   # user_input 초기화
+    global user_output
+    global user_output2
+
     # user_output = API.bard_api(user_input)
     user_output='바드답안'
     # user_output2 = API.palm_api_ko(user_input)
@@ -32,18 +38,24 @@ def save_user_input(request):
 def save_user_output(request):
 
     global user_input
-   
+    global user_output
+    global user_output2
     
     if request.method == 'POST':
-        user_output='hhhhehhhe'
-
+        which=request.POST.get('answer')
+        if which=='left':
+            answer=user_output
+        else:
+            answer=user_output2
         if user_input:
+            print(user_input,"OHOHOHOHOH",answer)
             # user_input을 데이터베이스에 저장하거나 다른 작업을 수행
-            QA = Question_Answer(Question=user_input,Answer=user_output)
+            QA = Question_Answer(Question=user_input,Answer=answer)
             QA.save()
             
-            return render(request, 'main/index2.html',{'user_input': user_input, 'user_output':user_output})  # 사용자 입력을 context에 추가
-    return render(request, 'main/index2.html', {'user_input': user_input,'user_output':user_output})  # POST 요청이 아닌 경우 또는 user_input이 없는 경우에 대한 응답 
+            
+            return render(request, 'main/index2.html',{'user_input': user_input, 'user_output':answer})  # 사용자 입력을 context에 추가
+    return render(request, 'main/index2.html', {'user_input': user_input,'user_output':answer})  # POST 요청이 아닌 경우 또는 user_input이 없는 경우에 대한 응답 
 
 # def load_data(request):
 #     global user_input
