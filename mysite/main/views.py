5ef864de6743a5cd322d4eae1530c9968cea77b5
namnamu <skadlsrud8@naver.com>
2023-11-13 13:42:56 +0900

    ask english answer english

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,7 +4,7 @@ from .models import Company
 from .models import QnA, Question_Answer
 from django.http import HttpResponse
 from django.template import loader
-from main.API import palm_api_ko 
+from main.API import palm_api_ko ,palm_api_en
 # 네이버 papago_api 번역
 from main.papago_translate import *
 
@@ -35,15 +35,19 @@ def save_user_input(request):
     global user_output2
 
     print("두번째페이지")
-    # user_output = bard_api(user_input)
-    # user_output='바드답안'
-    palm_answer = palm_api_ko(user_input)
-    user_output2='우측답안'
+    
     print("API완료")
     if request.method == 'POST':
         user_input = request.POST.get('user_input')  # POST 요청에서 user_input 데이터 가져오기
         if user_input:
+
+            print('유저 입력값:',user_input)
+            # user_output = bard_api(user_input)
+            user_output2='바드답안'
+            palm_answer = palm_api_en(user_input)
             
+            print('출력값:',palm_answer)
+
             return render(request, 'main/index.html', {'user_input': user_input,'user_output':palm_answer,'user_output2':user_output2})  # 사용자 입력을 context에 추가
     return render(request, 'main/index.html', { 'user_input': user_input, 'user_output':palm_answer,'user_output2':user_output2})  # POST 요청이 아닌 경우 또는 user_input이 없는 경우에 대한 응답
  
@@ -69,12 +73,5 @@ def save_user_output(request):
             return render(request, 'main/index2.html',{'user_input': user_input, 'user_output':answer})  # 사용자 입력을 context에 추가
     return render(request, 'main/index2.html', {'user_input': user_input,'user_output':answer})  # POST 요청이 아닌 경우 또는 user_input이 없는 경우에 대한 응답 
 
-# def load_data(request):
-#     global user_input
-#     qa = Question_Answer.objects.filter(Question=user_input) # 검색
-#     print(qa)
-#     template = loader.get_template('main/index.html')
-#     context["qa"] = qa # 화면에 보여줄 요소 추가
-#     return HttpResponse(template.render(context,request))# 화면상에 보인다.
 
 # 한국어 질문과 답변 (네이버 papago_api를 이용해 한국어 질문을 영어로 번역 후 palm에게 보내고 palm의 영어 답변을 한국어로 다시 번역)
